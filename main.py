16c6ed5847a19302f54d7f004499b7ef0667734f
Matthew Wallace <wallacm4@tcd.ie>
2024-09-12 21:24:23 +0100

    Added OAuth

--- main.py
+++ main.py
@@ -1,79 +1,44 @@
 import requests
 import time
-from requests_oauthlib import OAuth1Session
-import os
+import tweepy
 import json
 
 # URL to fetch products
 url = 'https://www.halseymusicstore.eu/products.json'
 previous_products = set()
 
-def twitter_auth():
-    consumer_key = '5xRUBnLbsL69owtta4WeFQdFx'
-    consumer_secret = '5wRguMrjCewzcKIw2fXLez9FgT0Ljb4IgOpknhNjCr9a72Fz2p'
+# Twitter API credentials
+API_KEY = '5xRUBnLbsL69owtta4WeFQdFx'
+API_SECRET_KEY = '5wRguMrjCewzcKIw2fXLez9FgT0Ljb4IgOpknhNjCr9a72Fz2p'
+ACCESS_TOKEN = '1834273643955732480-PzuN7oQWNOOEKkEwXJZMnjOmBrQejm'
+ACCESS_TOKEN_SECRET = 'QQRtYN4ULXxxdwqVVw9HpSPGmgRqu69nXUGMlWHY6lVdh'
+BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAAVwvwEAAAAAtaLP8Hxyk2yd096YrUXSxfAUb9M%3Dz1bY7dweFgrbLeaoTlQiaMLiHd8UxJmaSfFGR43K5tisfgA0Qs'
+CLIENT_ID = 'dVltRU5SZ1pHNndqVWwtSmtMekU6MTpjaQ'
+CLIENT_SECRET = 'UgmtU5SQMxBe8-10zlGycw7Hbqy5KHgJrov7gWpNSfZYgf_Qor'
 
-    request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
-    oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+# Set up tweepy client for OAuth 2.0 User Context
+client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
+def tweet(product):
     try:
-        fetch_response = oauth.fetch_request_token(request_token_url)
-    except ValueError:
-        print("There may have been an issue with the consumer_key or consumer_secret you entered.")
-        return None
+        title = product['title'].title()
+        price = product['variants'][0]['price']
+        link = f"https://www.halseymusicstore.eu/products/{product['handle']}"
+        tweet_text = f"🚨 NEW PRODUCT ALERT 🚨\n{title} - €{price}\n {link}"
+        
+        response = client.create_tweet(text=tweet_text)
 
-    resource_owner_key = fetch_response.get("oauth_token")
-    resource_owner_secret = fetch_response.get("oauth_token_secret")
-    print("Got OAuth token: %s" % resource_owner_key)
+        if response.errors:
+            raise Exception(f"Request returned an error: {response.errors}")
 
-    # Get authorization
-    base_authorization_url = "https://api.twitter.com/oauth/authorize"
-    authorization_url = oauth.authorization_url(base_authorization_url)
-    print("Please go here and authorize: %s" % authorization_url)
-    verifier = input("Paste the PIN here: ")
+        print(f"Tweeted: {tweet_text}")
+        print(json.dumps(response.data, indent=4, sort_keys=True))
+    except Exception as e:
+        print(f"Error tweeting: {e}")
 
-    # Get the access token
-    access_token_url = "https://api.twitter.com/oauth/access_token"
-    oauth = OAuth1Session(
-        consumer_key,
-        client_secret=consumer_secret,
-        resource_owner_key=resource_owner_key,
-        resource_owner_secret=resource_owner_secret,
-        verifier=verifier,
-    )
-    oauth_tokens = oauth.fetch_access_token(access_token_url)
-
-    access_token = oauth_tokens["oauth_token"]
-    access_token_secret = oauth_tokens["oauth_token_secret"]
-
-    # Return the authenticated session
-    return OAuth1Session(
-        consumer_key,
-        client_secret=consumer_secret,
-        resource_owner_key=access_token,
-        resource_owner_secret=access_token_secret,
-    )
-
-def tweet(oauth, product):
-    payload = {"text": "New Product Added: " + product}
-
-    # Making the request
-    response = oauth.post(
-        "https://api.twitter.com/2/tweets",
-        json=payload,
-    )
-
-    if response.status_code != 201:
-        raise Exception(
-            "Request returned an error: {} {}".format(response.status_code, response.text)
-        )
-
-    print("Response code: {}".format(response.status_code))
-
-    # Saving the response as JSON
-    json_response = response.json()
-    print(json.dumps(json_response, indent=4, sort_keys=True))
-
-def check_for_new_products(oauth):
+def check_for_new_products():
     global previous_products
     try:
         r = requests.get(url)
@@ -83,22 +48,19 @@ def check_for_new_products(oauth):
         current_products = set(item['title'] for item in data['products'])
         
         new_products = current_products - previous_products
-        for product in new_products:
-            print(f"New product added: {product}")
-            tweet(oauth, product)
-            break
+        for product in data['products']:
+            if product['title'] in new_products:
+                print(f"New product added: {product['title']}")
+                tweet(product)
+                break  # Exit the loop after the first iteration
         
         previous_products = current_products
     except requests.RequestException as e:
         print(f"Error fetching products: {e}")
 
 def start_monitoring(interval=60):
-    oauth = twitter_auth()
-    if oauth is None:
-        return
-
     while True:
-        check_for_new_products(oauth)
+        check_for_new_products()
         time.sleep(interval)
 
 # Call the start_monitoring function to begin monitoring every minute
